bf8370cddc75c5429baf6dea5b3c0de0b0174c7a
PR0M4XIMUS <125589845+PR0M4XIMUS@users.noreply.github.com>
2025-04-09 02:39:07 +0300

    Initial Commit

---
+++ shiot/shiotApp.swift
@@ -0,0 +1,17 @@
+//
+//  shiotApp.swift
+//  shiot
+//
+//  Created by Adrian Saca on 09.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct shiotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

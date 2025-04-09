620df644a0b84896cd128e972ac1d377c6c9a46f
PR0M4XIMUS <125589845+PR0M4XIMUS@users.noreply.github.com>
2025-04-09 03:32:49 +0300

    created shiot app

--- shiot/ContentView.swift
+++ shiot/ContentView.swift
@@ -1,24 +1,86 @@
-//
-//  ContentView.swift
-//  shiot
-//
-//  Created by Adrian Saca on 09.04.2025.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            VStack {
+                
+                
+                Text("BELOT")
+                    .font(.system(size: 60, weight: .bold, design: .default))
+                    .foregroundColor(.green)
+                
+                Text("$H!ØT")
+                    .font(.system(size: 35, weight: .bold, design: .default))
+                    .foregroundColor(.red)
+                
+                
+            }.padding(40)
+            
+                        
+            VStack {
+                
+                
+                
+                Button(action: {
+                    
+                }) {
+                    Text("2 Players").foregroundStyle(Color(.black))
+                        .padding()
+                        .background(Color(.gray))
+                        .cornerRadius(15)
+                }.padding()
+                
+                
+                
+                Button(action: {
+                    
+                }) {
+                    Text("3 Players").foregroundStyle(Color(.black))
+                        .padding()
+                        .background(Color(.gray))
+                        .cornerRadius(15)
+                }.padding()
+                
+                Button(action: {
+                    
+                }) {
+                    Text("4 Players").foregroundStyle(Color(.black))
+                        .padding()
+                        .background(Color(.gray))
+                        .cornerRadius(15)
+                }.padding()
+                
+                HStack {
+                    Spacer()
+                    Button(action: {
+                      print("Button tapped!")
+                      }) {
+                      Image(systemName: "gear.circle.fill")
+                      .font(.largeTitle)
+                      .foregroundColor(.blue)
+                      }.padding()
+                    
+                    
+                    Button(action: {
+                      print("Button tapped!")
+                      }) {
+                      Image(systemName: "questionmark.circle.fill")
+                      .font(.largeTitle)
+                      .foregroundColor(.blue)
+                      }.padding()
+                    Spacer()
+                }.padding()
+            }
+            Spacer()
+            Spacer()
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
 }
+
+
